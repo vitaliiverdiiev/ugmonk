6f7bc57218e3ca608cef68768ed47515df926b4b
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2021-06-12 01:19:35 +0300

    Completed mobile version. Minor changes are required. Need to fix mobile nav. Reorginize media queries files.

--- src/javascript/burger.js
+++ src/javascript/burger.js
@@ -1,6 +1,6 @@
 document.querySelector(".burger").addEventListener("click", function (e) {
 	this.classList.toggle("burger--active");
-	document.querySelector(".nav__items").classList.toggle("nav__items--active");
+	document.querySelector(".nav").classList.toggle("nav--active");
 
 	if (this.classList.contains("burger--active") && document.querySelector(".nav__items").classList.contains("nav__items--active")) {
 		document.body.style.overflow = "hidden"
@@ -19,3 +19,25 @@ document.querySelector("ul.nav__items").addEventListener("click", function (e) {
 		null
 	}
 })
+
+// document.querySelector(".burger").addEventListener("click", function (e) {
+// 	this.classList.toggle("burger--active");
+// 	document.querySelector(".nav__items").classList.toggle("nav__items--active");
+
+// 	if (this.classList.contains("burger--active") && document.querySelector(".nav__items").classList.contains("nav__items--active")) {
+// 		document.body.style.overflow = "hidden"
+// 	} else {
+// 		document.body.style.overflowY = "auto"
+// 	}
+// })
+
+// document.querySelector("ul.nav__items").addEventListener("click", function (e) {
+// 	document.querySelector(".burger").classList.toggle("burger--active");
+// 	this.classList.toggle("nav__items--active");
+
+// 	if (!this.classList.contains("burger--active") && !document.querySelector(".nav__items").classList.contains("nav__items--active")) {
+// 		document.body.style.overflowY = "auto"
+// 	} else {
+// 		null
+// 	}
+// })
